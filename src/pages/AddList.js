8fb2d0a13eab2b183f5d84477e3b421e787b8a90
Tolga <gnydnntolga@gmail.com>
2021-12-08 17:53:56 +0300

    Liste form sayfası hazırlandı.

--- src/pages/AddList.js
+++ src/pages/AddList.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {
+    ListFormBox,
+    ListFormHead,
+    ListForm,
+    ListFormGroup,
+    ListFormInput,
+    ListFormLabel,
+    ListButtonBox,
+    ListFormButton
+    } from '../styled/AddListStyled'
+
 
 function AddList() {
+
+    const [listTitle, setlistTitle] = useState("");
+    const [listLink, setlistLink] = useState("");
+
+    function addingfunc(e) {
+        e.preventDefault();
+        if (!listTitle || !listLink) {
+          alert("Kindly fill both Inputs");
+        } else {
+            setlistTitle("");
+            setlistLink("");
+        }
+      }
+
+      console.log(listTitle, listLink)
+
     return (
-        <div>
-            Liste Eklenecek
-        </div>
+        <ListFormBox>
+            <ListFormHead>Add New Link</ListFormHead>
+            <ListForm onSubmit={addingfunc}>
+                <ListFormGroup>
+                    <ListFormLabel> Link Name: </ListFormLabel>
+                    <ListFormInput value={listTitle} type="text" onChange={(e) => setlistTitle(e.target.value)} placeholder="e.g Alphabet"/>
+                </ListFormGroup>
+                <ListFormGroup>
+                    <ListFormLabel> Link URL: </ListFormLabel>
+                    <ListFormInput value={listLink} type="text" onChange={(e) => setlistLink(e.target.value)} placeholder="e.g https://abc.xyz"/>
+                </ListFormGroup>
+                <ListButtonBox>
+                    <ListFormButton>Link Add</ListFormButton>
+                </ListButtonBox>
+            </ListForm>
+        </ListFormBox>
     )
 }
 
---
+++ src/styled/AddListStyled.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+
+
+export const ListFormBox = styled.div`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 350px;
+`
+
+export const ListFormHead = styled.h1`
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: rgb(72, 72, 72);
+    text-align: center;
+    margin-bottom: 1rem;
+`
+
+export const ListForm = styled.form`
+    padding:2rem;
+    border:solid 1px #e5e5e5;
+    border-radius: 1rem;
+`
+
+export const ListFormGroup = styled.div`
+    margin-bottom: 1rem;
+`
+
+export const ListFormLabel = styled.label`
+    font-weight: 600;
+    font-size:0.785rem;
+    color: rgb(72, 72, 72);
+`
+
+
+export const ListFormInput = styled.input`
+    background:#e5e5e5;
+    border:0;
+    height: 55px;
+    font-size: 0.765rem;
+    display: block;
+    width: 100%;
+    color: rgb(72, 72, 72);
+    border-left-width: solid 3px rgb(255, 96, 0);
+    padding-left: 1rem;
+    margin-top:0.5rem;
+    border-radius:0.4rem;
+    &:focus {
+        border-color: rgb(255, 96, 0);
+    }
+`
+export const ListButtonBox = styled.div`
+    text-align:center;
+`
+export const ListFormButton = styled.button`
+    background: none;
+    font-size:0.865rem;
+    outline: none;
+    border:solid 1px #e5e5e5;
+    color: rgb(72, 72, 72);
+    padding:12px 30px;
+    border-radius:0.4rem;
+    width: 100%;
+    margin-top: 10px;
+    cursor:pointer;
+    transition: all .5s ease;
+    font-weight: 600;
+    width: 100%;
+    &:hover {
+        background: rgb(255, 96, 0);
+        color:#ffffff;
+        border-color:transparent;
+    }
+`
